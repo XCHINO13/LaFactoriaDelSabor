6ef165f47aeb11c28d207c5f187dfa6236161a45
JuanDQ1 <142171820+JuanDQ1@users.noreply.github.com>
2023-08-23 22:54:32 -0500

    Registro de datos en la base de usuario

--- backEnd/routes/user.js
+++ backEnd/routes/user.js
@@ -2,6 +2,32 @@ const express = require('express');
 const connection = require('../connection');
 const router = express.Router();
 
-router.post
+router.post('/signup',(req,res) => {
+    let user = req.body;
+    query = "select email,password,role,status from user where email=?"
+    connection.query(query,[user.email],(err,results)=>{
+        if(!err){
+            if(results.length <=0){
+                query = "insert into user(name,contactNumber,email,password,status,role) values(?,?,?,?,'false','user')";
+                connection.query(query,[user.name,user.contactNumber,user.email,user.password],(err,results) =>{
+                    if(!err){
+                        return res.status(200).json({message: "Usuario Registrado con exito"});
+                    }
+                    else{
+                        return res.status(500).json(err);
+                    }
+                })
+            }
+            else{
+                return res.status(400).json({message: "Email Registrado en la base de datos"});
+            }
+        }
+        else{
+            return res.status(500).json(err);
+        }
+        
+    })
+    
+})
 
 module.exports = router;
